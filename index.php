4c1df9e7f00de114dd13cc681a2f6a2afa8482cc
= <=>
2021-10-07 11:56:03 +0700

    login

--- index.php
+++ index.php
@@ -69,9 +69,7 @@
 
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
-						<a href="home.php">
 							Login
-						</a>
 						</button>
 					</div>
 					
